75d490e2da69d1d42dafa0f4f5fbab467934c921
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-25 14:47:24 -0400

    Update and rename __init__.py to __init__.py

--- robo_utils/__init__.py
+++ robo_utils/__init__.py
@@ -1,17 +1,19 @@
 
 """
-basic-pid - PID Controller for discretized time
+robo-utils  - useful functions for mobile robotics
 """
 
 # flit uses this as one source version #
 # note: pep whatever doesnt like > 'x.y.<number>c'
 # for example: 0.01.08d doesnt fly building from pyproject.toml
 
-__version__ = '0.01.10b'
+__version__ = '0.01.01'
 
 # import acts to export 
 
-from .pid import BasicPid
-from .pid import BasicPid as Pid
+from .roboutils import imap
+from .roboutils import fmap
+
+
 
 
