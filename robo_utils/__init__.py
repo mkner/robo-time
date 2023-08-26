4f1638736372203592ba06e7bf07df4340fc44cc
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-26 15:27:51 -0400

    Update __init__.py

--- robo_utils/__init__.py
+++ robo_utils/__init__.py
@@ -19,6 +19,21 @@ from .roboutils import rad2deg
 from .roboutils import deg2rad
 
 from .roboutils import boundTo2pi
+from .roboutils import bound2pi
+    
+from .roboutils import boundArctan
+from .roboutils import boundAtanDeg
+from .roboutils import boundAtanRad
+
+from .roboutils import getAngleFromTo
+
+from .roboutils import getDistanceFromTo
+from .roboutils import getDistanceBetween
+from .roboutils import getPointDistanceFrom
+
+from .roboutils import mps2kmph
+from .roboutils import mps2mph
+
 
 
 
