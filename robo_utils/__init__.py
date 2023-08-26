92936598911a75757b78ecf6cd360a5aedc87daa
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-26 15:13:31 -0400

    Update __init__.py

--- robo_utils/__init__.py
+++ robo_utils/__init__.py
@@ -7,12 +7,17 @@ robo-utils  - useful functions for mobile robotics
 # note: pep whatever doesnt like > 'x.y.<number>c'
 # for example: 0.01.08d doesnt fly building from pyproject.toml
 
-__version__ = '0.01.03b'
+__version__ = '0.01.03b1'
 
 # import acts to export 
 
+import .roboutils as roboutils # make it avail like this also
+
 from .roboutils import imap
 from .roboutils import fmap
+from .roboutils import rad2deg
+from .roboutils import deg2rad
+
 
 
 
