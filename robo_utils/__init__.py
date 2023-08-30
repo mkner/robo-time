7dec455b4f92cd5c395be136d2f481ae0783d2bd
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-08-30 15:36:46 -0400

    Update __init__.py

--- robo_utils/__init__.py
+++ robo_utils/__init__.py
@@ -7,7 +7,7 @@ robo-utils  - useful functions for mobile robotics
 # note: pep whatever doesnt like > 'x.y.<number>c'
 # for example: 0.01.08d doesnt fly building from pyproject.toml
 
-__version__ = '0.01.03c4'
+__version__ = '0.02.01' #prev '0.01.03c4'
 
 # import acts to export 
 
