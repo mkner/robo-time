919fca8a8d7567d751e013f92069eea3cd894b29
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-02 16:23:10 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+DISTNAME = "robo-clocks"
+DESCRIPTION = "clocks, timing, timstamping"
+
+VERSION = roboclocks.__version__  # gets from init??
+
+# import a restricted version of roboclocks
+# does not need the compiled code??
+
+import roboclocks 
+import import roboclocks.time as robotime
+
