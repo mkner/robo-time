f16642c72a956d2dc4d63632bc04049eff9f6914
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-02 17:15:45 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 DISTNAME = "robo-clocks"
 DESCRIPTION = "clocks, timing, timstamping"
 
-VERSION = roboclocks.__version__  # gets from init??
+VERSION = robo_clocks.__version__  # gets from init??
 
 # import a restricted version of roboclocks
 # does not need the compiled code??
