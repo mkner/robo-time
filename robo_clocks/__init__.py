402b44196ccb66951118a489a9acf81909bfafbf
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-02 17:24:52 -0400

    Update __init__.py

--- robo_clocks/__init__.py
+++ robo_clocks/__init__.py
@@ -5,7 +5,7 @@ robo-clocks  - useful functions for mobile robotics
 
 # flit uses this as one source version #
 
-__version__ = '0.01.01b' 
+__version__ = '0.01.01c' 
 
 from . import clocks
 from . import time
