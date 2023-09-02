a39001c5809d10cecd44452f6a234f6e797a577b
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-02 14:34:43 -0400

    Update __init__.py

--- robo_clocks/__init__.py
+++ robo_clocks/__init__.py
@@ -1,19 +1,16 @@
 
 """
-robo-utils  - useful functions for mobile robotics
+robo-clocks  - useful functions for mobile robotics
 """
 
 # flit uses this as one source version #
-# note: pep whatever doesnt like > 'x.y.<number>c'
-# for example: 0.01.08d doesnt fly building from pyproject.toml
 
-__version__ = '0.02.01' #prev '0.01.03c4'
+__version__ = '0.01.01a' 
 
-# import acts to export 
+from . import roboclocks
+from roboclocks import Clock
 
-from . import roboutils #
 
-from .roboutils import *
 
 
 
