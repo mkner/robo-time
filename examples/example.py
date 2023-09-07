e7583746965e01a876ad355a44f0c18df7202a4a
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-07 17:01:10 -0400

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -1,10 +1,6 @@
 # examples
 
+from roboclocks import Clock
+from roboclocks.time import delay
 
-from robo_utils import imap
 
-imap(50,0,100,0,100)
-Out[18]: 50
-
-imap(50,0,100,0,255)
-Out[19]: 127
