19676c78fb299ef75d0396ffbbc4cfd3d41f2a9e
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-08 18:24:42 -0400

    Update __init__.py

--- roboclocks/__init__.py
+++ roboclocks/__init__.py
@@ -8,9 +8,13 @@ __version__ = '0.01.02c'
 from . import clocks
 from . import time
 
+from .clocks import Clock
+from .clocks import SystemClock
+from .clocks import WorldClock
+
 from .clocks import *
 from .time import *
-#
+
 
 
 
