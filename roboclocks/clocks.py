a21103ba3f08a17b60b59a299615b91d2c03d04d
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-09 15:17:02 -0400

    Update clocks.py

--- roboclocks/clocks.py
+++ roboclocks/clocks.py
@@ -2257,28 +2257,3 @@ class SyncSlaveClock(Clock):
 #/**********************************************/
 
 
-############################################################
-############################################################
-"""
-import numpy as np
-
-import math as m
-import math
-
-import time
-#import datetime
-from datetime import datetime, timezone
-from datetime import timedelta
-import calendar
-import ntplib
-
-#from robotime.time import *
-#from roboclocks.time import *
-
-from robobase import Object
-
-# local build here
-from time import *
-"""
-
-
