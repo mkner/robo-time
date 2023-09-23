c55166cf52c6232d265052047189610088ab7cd4
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-23 14:47:08 -0400

    Update __init__.py

--- robotime/__init__.py
+++ robotime/__init__.py
@@ -12,6 +12,9 @@ from .clocks import Clock
 from .clocks import SystemClock
 from .clocks import WorldClock
 
+# also
+from .clocks import Clock as UptimeClock
+
 from .clocks import *
 from .time import *
 
