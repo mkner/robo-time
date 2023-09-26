415bea2e076b024af3584f7d41e911754bfcfc5f
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-26 18:34:47 -0400

    Update test.py

--- robotime/test.py
+++ robotime/test.py
@@ -9,30 +9,50 @@
 #
 # (c) 2022 - 2023  Mike Knerr
 #
-# 
+#
+#
+#
+# robotime - clocks - timestamps
+# for mobile robotics and related applications
+#
+# quickcheck
+#
+# short form calls to check
+# basic integrity of functionality
+#
+# (c) 2023  Mike Knerr
+#
+#
 
-#from datetime import datetime
-#from datetime import timezone
-#import calendar
-#import time
-
+# for vers
+import robobase
 import robotime
+import roboutils
+
+### working or installed HERE
+
+#from clocks import Clock
+from robotime.clocks import Clock
+
+#from clocks import SystemClock
+from robotime.clocks import SystemClock
+
+#from clocks import WorldClock
+from robotime.clocks import WorldClock
+
+### MAKE CLOCKS ###
+
+c = Clock()
+sc = SystemClock()
+wc = WorldClock()
+
+# CK THAT WORKS
+from robotime import UptimeClock
+uc = UptimeClock()
+
 from robotime.time import delay
 
-#robot = False
 #from robot import Robot
-#robot = robot
-
-from robotime.clocks import Clock
-#from clocks import Clock
-c=Clock()
-
-from robotime.clocks import SystemClock
-#from clocks import SystemClock
-
-from robotime.clocks import WorldClock
-#from clocks import WorldClock# from local dir working version
-wc = WorldClock()
 
 
 def up():
@@ -54,6 +74,7 @@ def time():
     c.time()
     wc.time()
     sc.time()
+    
 
 def today():
     c.today()
@@ -78,6 +99,49 @@ def tsr():
     print(wc.getTimestampStr())
     print(sc.getTimestampStr())
 
+def tsi():
+    print(c.getTimestampInt())
+    print(wc.getTimestampInt())
+    print(sc.getTimestampInt())
+
+def tsf():
+    print(c.getTimestampFp())
+    print(wc.getTimestampFp())
+    print(sc.getTimestampFp())
+
+def tsfp():
+    print(c.getTimestampFp())
+    print(wc.getTimestampFp())
+    print(sc.getTimestampFp())
+
+def upfp():
+    print(c.getUptimeFp())
+    print(wc.getUptimeFp())
+    print(sc.getUptimeFp())
+ 
+    
+def upfploop(count):
+    count = abs(count)
+    print()
+    for i in range(count):
+        upfp()
+        c.delay(1000)
+        print()
+
+def tsfploop(count):
+    count = abs(count)
+    print()
+    for i in range(count):
+        tsfp()
+        c.delay(1000)
+        print()
+
+def epoch():
+    c.epoch()
+    wc.epoch()
+    sc.epoch()
+
+
 
 def setTstype(ts):
     c.setTimestampFormat(ts)
@@ -101,6 +165,54 @@ def setlocal():
 def setutc():
     setTstype('iso-utc')
     
+    
+def setcompon():
+    c.setTimestampCompressedOn()
+    sc.setTimestampCompressedOn()
+    wc.setTimestampCompressedOn()
+
+def setcompoff():
+    c.setTimestampCompressedOff()
+    sc.setTimestampCompressedOff()
+    wc.setTimestampCompressedOff()
+
+def ztailon():
+    c.setZtailOn()
+    wc.setZtailOn()
+    sc.setZtailOn()
+    
+def ztailoff():
+    c.setZtailOff()
+    wc.setZtailOff()
+    sc.setZtailOff()
+
+
+def setresMicros():
+    c.setTimestampRes('micro')
+    wc.setTimestampRes('micro')
+    sc.setTimestampRes('micro')
+    
+def setresMillis():
+    c.setTimestampRes('milli')
+    wc.setTimestampRes('milli')
+    sc.setTimestampRes('milli')    
+
+def setresOff():
+    c.setTimestampRes('none')
+    wc.setTimestampRes('none')
+    sc.setTimestampRes('none')    
+
+
+def vers():
+    print()
+    print("robo-utils version "+str(roboutils.__version__))
+    print()
+    print("robo-base version "+str(robobase.__version__))
+    print()
+    print("robo-time version "+str(robotime.__version__))
+    print()
+
+
 
 def roll():
     print()
@@ -109,7 +221,7 @@ def roll():
     print("format")
     getTstype()
     print()
-    delay(2000)
+    delay(1000)
     
     print("up")
     up()
@@ -145,3 +257,19 @@ def roll():
     tsr()
     print()
     delay(2000)
+    
+    print("getTimestampInt()")
+    tsi()
+    print()
+    delay(2000)
+
+    print("getTimestampFp()")
+    tsf()
+    print()
+    delay(2000)
+    
+    print("getUptimeFp()")
+    upfp()
+    print()
+    delay(2000)
+    
