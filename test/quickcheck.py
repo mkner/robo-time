7d3551e96d3d847c8bcad5717ab593a10a39ea42
Mike Knerr <53237403+mkner@users.noreply.github.com>
2023-09-26 16:42:56 -0400

    Update quickcheck.py

--- test/quickcheck.py
+++ test/quickcheck.py
@@ -1,1 +1,13 @@
-
+#
+#
+# robotime - clocks - timestamps
+# for mobile robotics and related applications
+#
+# quickcheck
+#
+# short form calls to check
+# basic integrity of functionality
+#
+# (c) 2023  Mike Knerr
+#
+#
